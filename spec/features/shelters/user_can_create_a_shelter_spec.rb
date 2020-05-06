40e2b5f33575f3c9f622e12ea2c06d6038299bff
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-05 18:58:23 -0600

    Created create shelter test

--- spec/features/shelters/user_can_create_a_shelter_spec.rb
+++ spec/features/shelters/user_can_create_a_shelter_spec.rb
@@ -1,6 +1,6 @@
-require 'rails helper'
+require 'rails_helper'
 
-RSpec.describe "shelter create page", :type :feature do
+RSpec.describe "shelter create page", type: :feature do
   it "can create shelters" do
     shelter = Shelter.create(name: "Dog Shelter",
                              address: "12980 Grover Drive",
@@ -9,14 +9,19 @@ RSpec.describe "shelter create page", :type :feature do
                              zip: 74578)
 
     visit "/shelters"
-    click_link('new shelter')
+    click_link('New Shelter')
     expect(current_path).to_eql('/shelters.new')
-    # fill_in :name, with: "Dog Shelter"
-    # fill_in :address, with: "12980 Grover Drive"
-    # fill_in :city, with: "Doggy Vale"
-    # fill_in :state, with: "Colorado"
-    # fill_in :zip, with: 74578
-    # click_on 'submit'
-    # visit '/shelters'
+    fill_in :name, with: "Dog Shelter"
+    fill_in :address, with: "12980 Grover Drive"
+    fill_in :city, with: "Doggy Vale"
+    fill_in :state, with: "Colorado"
+    fill_in :zip, with: 74578
+    click_on 'submit'
+    expect(current_path).to have_content('/shelters')
+    expect(current_path).to have_content('Dog Shelter')
+    expect(current_path).to have_content('2980 Grover Drive')
+    expect(current_path).to have_content('Doggy Vale')
+    expect(current_path).to have_content('Colorado')
+    expect(current_path).to have_content(74578)
   end
 end
