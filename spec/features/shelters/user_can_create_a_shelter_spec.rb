bc1ec7b73126f2ed2c2065cbecfa02a11422c1e9
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-05 18:05:58 -0600

    created link test

--- spec/features/shelters/user_can_create_a_shelter_spec.rb
+++ spec/features/shelters/user_can_create_a_shelter_spec.rb
@@ -8,6 +8,15 @@ RSpec.describe "shelter create page", :type :feature do
                              state: "Colorado",
                              zip: 74578)
 
-    
-
+    visit "/shelters"
+    click_link('new shelter')
+    expect(current_path).to_eql('/shelters.new')
+    # fill_in :name, with: "Dog Shelter"
+    # fill_in :address, with: "12980 Grover Drive"
+    # fill_in :city, with: "Doggy Vale"
+    # fill_in :state, with: "Colorado"
+    # fill_in :zip, with: 74578
+    # click_on 'submit'
+    # visit '/shelters'
   end
+end
