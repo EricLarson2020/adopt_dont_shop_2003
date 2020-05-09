2d447e23c2b5f39a35329b5ba2197fa9dfa9613f
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-09 14:43:08 -0600

    Created test for delete pets

---
+++ spec/features/pets/user_can_delete_pet_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.describe "Delete Pet Page", type: :feature do
+  it "Can Delete A Pet From Pet Show Page" do
+
+    shelter_1 = Shelter.create({name: "Happy Shelter",
+                             address: "12980 Grover Drive",
+                             city: "Doggy Vale",
+                             state: "Colorado",
+                             zip: 74578})
+
+    pet = Pet.create({
+      image: "Happy Url",
+      name: "Happy",
+      approximate_age: 2,
+      sex: "Male",
+      shelter_id: shelter_1.id
+      })
+
+    visit "/pets/#{pet.id}"
+    expect(page).to have_content(pet.image)
+    expect(page).to have_content(pet.name)
+    expect(page).to have_content(pet.approximate_age)
+    expect(page).to have_content(pet.sex)
+    click_link("Delete Pet")
+    expect(current_path).to eql("/pets/#{pet.id}")
+    expect(page).to_not have_content(pet.image)
+    expect(page).to_not have_content(pet.name)
+    expect(page).to_not have_content(pet.approximate_age)
+    expect(page).to_not have_content(pet.sex)
+    expect(page).to_not have_content(pet.shleter_id)
+  end
+
+end
