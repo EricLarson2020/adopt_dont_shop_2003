590b694284cd557aea85ac6a7dcab21fc528acfa
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-09 14:23:59 -0600

    created pets update route

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,7 @@ Rails.application.routes.draw do
   get '/shelters/:id/pets/new', to: 'pets#new'
   post '/shelters/:id/pets', to: 'pets#create'
   get '/pets/:id/edit', to: 'pets#edit'
+  patch '/pets/:id', to: 'pets#update'
   get '/pets/:id', to: 'pets#show'
 
 end
