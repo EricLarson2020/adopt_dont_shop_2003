23dc615f4161687269448a91e61dde3ed866682e
Eric Larson <ericlarsonbroom@gmail.com>
2020-05-10 07:13:22 -0600

    created link to delete pet from pet index

--- spec/features/pets/user_can_delete_pet_from_index_spec.rb
+++ spec/features/pets/user_can_delete_pet_from_index_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe "Shelter Index Page", type: :feature do
   it "can delete pet from index page" do
 
+    shelter_1 = Shelter.create({name: "Happy Shelter",
+                             address: "12980 Grover Drive",
+                             city: "Doggy Vale",
+                             state: "Colorado",
+                             zip: 74578})
 
     pet = Pet.create({
       image: "Happy Url",
@@ -12,6 +17,8 @@ RSpec.describe "Shelter Index Page", type: :feature do
       shelter_id: shelter_1.id
       })
 
- visit '/pets'
- click_link("Delete Pet #{pet.id}")
- expect(current_path).to eql('/pets')
+   visit '/pets'
+   click_link("Delete Pet #{pet.id}")
+   expect(current_path).to eql('/pets')
+ end
+end
